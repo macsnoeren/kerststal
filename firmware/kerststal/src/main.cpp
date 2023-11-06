4f053af4cb1fcc91e56fba1d95753ca46e7248c7
Maurice Snoeren <macsnoeren@gmail.com>
2023-11-06 09:02:44 +0100

    Added a first skeleton and some images of the project as documentation.

---
+++ firmware/kerststal/src/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Includes of the Arduino framework
+#include <Arduino.h>
+#include <driver/ledc.h>
+#include <esp_err.h>
+
+// Includes of the added libraries
+#include <NeoPixelBus.h> // https://github.com/Makuna/NeoPixelBus
+
+void setup() {
+
+
+}
+
+void loop() {
+
+}
